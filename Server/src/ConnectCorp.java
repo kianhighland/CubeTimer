dc3c38e7bee71b0551f76ee54874096d528f4d4d
kianhighland <kianhighland@gmail.com>
2016-02-25 07:39:38 -0700

    made connectRunner and connectCorp classes

---
+++ Server/src/ConnectCorp.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+public class ConnectCorp{
+
+    private static String sCorp = "Corp";
+
+    private Corp corp;
+    private Output output;
+    private ServerSocket serverSocket;
+    private DataInputStream firstIn;
+    private DataInputStream firstOut;
+
+    public ConnectRunner(Output outputIn, ServerSocket serverSocketIn){
+
+        output = outputIn;
+        serverSocket = serverSocketIn;
+    }
+
+    public void run(){
+
+        Socket socket = serverSocket.accept();
+        firstIn = new DataInputStream(socket.getInputStream());
+        String playertype = firstIn.readUTF();
+        if(playertype.matches(sCorp)){
+
+            connectCorp(socket);
+        }
+        else{
+            
+            firstOut = new DataOutputStream(socket.getOutputStream());
+            firstOut.writeUTF("Sorry, " + playertype + " did not match "
+                + sCorp);
+            firstOut = null;
+            run();
+        }
+    }
+
+    public void connectCorp(Socket socket){
+
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        out.writeUTF("You have succesfully connected as Corp");
+        corp = new Corp(out, in, output);
+        output.setCorp(Corp);
+        Thread tCorp = new Thread(corp);
+        tRunner.start();
+    }
+}
---
+++ Server/src/ConnectRunner.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+public class ConnectRunner{
+
+    private final String sRunner = "Runner";
+
+    private Runner runner;
+    private Output output;
+    private ServerSocket serverSocket;
+    private DataInputStream firstIn;
+    private DataInputStream firstOut;
+
+    public ConnectRunner(Output outputIn, ServerSocket serverSocketIn){
+
+        output = outputIn;
+        serverSocket = serverSocketIn;
+    }
+
+    public void run(){
+
+        Socket socket = serverSocket.accept();
+        firstIn = new DataInputStream(socket.getInputStream());
+        String playertype = firstIn.readUTF();
+        if(playertype.matches(sRunner)){
+
+            connectRunner(socket);
+        }
+        else{
+            
+            firstOut = new DataOutputStream(socket.getOutputStream());
+            firstOut.writeUTF("Sorry, " + playertype + " did not match "
+                + sRunner);
+            firstOut = null;
+            run();
+        }
+    }
+
+    public void connectRunner(Socket socket){
+
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        out.writeUTF("You have succesfully connected as Runner");
+        runner = new Runner(out, in, output);
+        output.setRunner(runner);
+        Thread tRunner = new Thread(runner);
+        tRunner.start();
+    }
+}
