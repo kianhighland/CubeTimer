b85c42faff981ac7c18a8a6e2fc42adce82b28ff
kianhighland <kianhighland@gmail.com>
2016-03-06 18:42:19 -0700

    Did a little in the constants class to return it to unbold when I don't want it to be bold

--- Corp/src/Constants.java
+++ Corp/src/Constants.java
@@ -1,7 +1,6 @@
 public class Constants{
 
-    public static final String normalText = (char)27 + "[0m" + (char)27
-        + "[40m" + (char)27 + "[37m";
+    public static final String normalText = (char)27 + "[0;40;37m";
     public static final String corp = "Corp";
     public static final String slash = "/";
     public static final String q = "q";
@@ -10,8 +9,7 @@ public class Constants{
         + (char)27 + "[32m";
     public static final String commandMode = (char)27 + "[35m" + corp
         + "|command> " + (char)27 + "[33m";
-    public static final String corpChat = (char)27 + "[0;36m";
-    public static final String corpActions = (char)27 + "[34m";
-    public static final String actionPronpts = (char)27 + "[34m" + (char)27
-        + "[1m";
+    public static final String corpChat = normalText + (char)27 + "[36m";
+    public static final String corpActions = normalText + (char)27 + "[34m";
+    public static final String actionPronpts = (char)27 + "[1;34m";
 }
--- Server/src/fields/Constants.java
+++ Server/src/fields/Constants.java
@@ -2,8 +2,7 @@ package fields;
 
 public class Constants {
 
-    public static final String normalText = (char)27 + "[0m" + (char)27
-        + "[40m" + (char)27 + "[37m";
+    public static final String normalText = (char)27 + "[0;40;37m";
     public static final String corp = "Corp";
     public static final String runner = "Runner";
     public static final String server = "Server";
@@ -13,6 +12,6 @@ public class Constants {
     public static final String c = "c";
     public static final String serverInput = (char)27 + "[1;36m"
         + "Server> " + (char)27 + "[1;32m";
-    public static final String serverChat =(char)27 + "[1;37m";
+    public static final String serverChat = (char)27 + "[1;37m";
     public static final String actionPrompts = (char)27 + "[1;30m";
 }
